680fa658cee60187ff91a2d5a16c791930d28033
ivanmogilevskij <Gytalin13>
2018-10-20 14:46:56 +0300

    Init site

---
+++ cms/global/global.js
@@ -0,0 +1,33 @@
+({
+	mainMenu: [
+		{
+			name: 'Наши услуги',
+			link: '#service'
+		},
+		{
+			name: 'Контакты',
+			link: '#contact'
+		},
+		{
+			name: '8 901 985 30 44',
+			link: 'tel:89019853044'
+		}
+	],
+	company_name: 'MOBI-TELL',
+	contacts: {
+		header: 'Наши контакты',
+		phone: [
+			{
+				number: '983044'
+			},
+			{
+				number: '8 901 985 30 44'
+			},
+			{
+				number: '8 920 145 02 34'
+			}
+		],
+		email: 'mobi-tell@mail.ru',
+		address: 'г.Ярославль, Ленинградский пр-т, 67,тц. «Алмаз»'
+	}
+})
